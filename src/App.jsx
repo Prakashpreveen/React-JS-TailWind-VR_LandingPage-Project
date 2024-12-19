a6b35f96280e6e777785b916e7640d5c5c1925c7
Divya Prakash <prakashpreveen@gmail.com>
2024-12-19 11:55:13 +0530

    added pricing

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import Navbar from "./components/Navbar";
 import HeroSection from "./components/HeroSection";
 import FeatureSection from "./components/FeatureSection";
+import Workflow from "./components/Workflow";
+import Pricing from "./components/Pricing";
 
 const App = () => {
   return (
@@ -10,8 +12,8 @@ const App = () => {
       <div className="max-w-7xl mx-auto pt-20 px-6">
         <HeroSection />
         <FeatureSection />
-        {/* <Workflow /> */}
-        {/* <Pricing /> */}
+        <Workflow />
+        <Pricing />
         {/* <Testimonials /> */}
         {/* <Footer /> */}
       </div>
